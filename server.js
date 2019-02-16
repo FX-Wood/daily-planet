7ff3db6c06a13d50ba03537142b3e2f5d8d3b46a
FX-Wood <fxwood2@gmail.com>
2019-02-15 17:39:13 -0800

    committing WIP for push to remote

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+
 
 app.use(express.static('static'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('root')
+    res.send('root');
 })
 
-app.listen(3000)
+app.get('/articles', function(req, res) {
+    const articles = JSON.parse(fs.readFileSync('static/articles.json'));
+    res.render('articles/index', {data: articles})
+});
+
+
+
+app.listen(3001)
