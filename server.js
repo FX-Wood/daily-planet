459c55c4cb074690e3da9f2ffd11fff5d74cd638
FX-Wood <fxwood2@gmail.com>
2019-02-15 22:14:36 -0800

    added sketches of routes

--- server.js
+++ server.js
@@ -1,21 +1,57 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
+const methodOverride = require('method-override')
 
 
+// helpers
+const path = 'static/articles.json'
+const getArticles = function() {
+    return JSON.parse(fs.readFileSync(path))
+}
+const setArticles = function(obj) {
+    fs.writeFileSync(path, JSON.stringify(obj), )
+}
 
-app.use(express.static('static'));
-app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.send('root');
+// init
+const app = express();
+app.listen(3000, function() {
+    console.log('Daily Planet serving on port 3000')
 })
 
-app.get('/articles', function(req, res) {
-    const articles = JSON.parse(fs.readFileSync('static/articles.json'));
-    res.render('articles/index', {data: articles})
+// middleware
+app.use(express.static('static'));
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+
+// root | GET 
+app.get('/', function(req, res) {
+    res.redirect('/articles');
 });
 
+// /articles | GET | PUT
+app.get('/articles', function(req, res) {
+    res.render('articles', {data: getArticles()})
+});
 
+app.post('/articles', function(req, res) {
+    
+    res.redirect('/articles');
+});
 
-app.listen(3001)
+// /articles/:id | GET | PUT | DELETE
+app.get('/articles/:id', function(req, res) {
+    res.render('articles/show');
+});
+
+app.put('/articles/:id', function(req, res) {
+    
+});
+
+app.delete('/articles/:id', function(req, res) {
+
+});
+
+// /widgets/new | GET
+
